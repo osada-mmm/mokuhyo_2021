035997c5b89f8f19bdba6543ac58c138871ebf7b
osada <osada-m@uni-com.com>
2021-10-18 15:06:36 +0900

    modify

--- app/frontend/pages/car/CarList.js
+++ app/frontend/pages/car/CarList.js
@@ -1,7 +1,10 @@
 import { any } from "prop-types";
 import React from "react"
 import Button from "@material-ui/core/Button";
+import Grid from "@material-ui/core/Grid";
 import { useHistory } from "react-router";
+
+
 
 const CarList = (props) => {
   const cars = props.car;
@@ -11,16 +14,17 @@ const CarList = (props) => {
     <div>
       {
         cars.map((car, idx) => (
-          <div key={idx}>
-            <label>{car.carId}</label>
-            <label>{car.maker}</label>
-            <label>{car.model}</label>
-            <label>{car.grade}</label>
-            <label>{car.price}</label>
-            <label>{car.navi}</label>
-            <label>{car.kawa}</label>
-            <label>{car.sr == "1" ? "サンルーフ有" : ""}</label>
-            <Button 
+          <div className="contents" key={idx}>
+            <Grid container>
+              <Grid item xs={1}><label>{car.carId}</label></Grid>
+              <Grid item xs={1}><label>{car.maker}</label></Grid>
+              <Grid item xs={1}><label>{car.model}</label></Grid>
+              <Grid item xs={1}><label>{car.grade}</label></Grid>
+              <Grid item xs={1}><label>{car.price}</label></Grid>
+              <Grid item xs={1}><label>{car.navi == "1" ? "ナビ有" : ""}</label></Grid>
+              <Grid item xs={1}><label>{car.kawa == "1" ? "革有" : ""}</label></Grid>
+              <Grid item xs={1}><label>{car.sr == "1" ? "サンルーフ有" : ""}</label></Grid>
+              <Button 
               type="button"
               variant="contained" 
               color="primary"
@@ -28,6 +32,7 @@ const CarList = (props) => {
             >
               詳細
             </Button>
+            </Grid>
           </div>
         ))
       }
